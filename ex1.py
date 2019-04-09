ac114762c91ee958e97c112ce2e58f9965f94fd0
tolany <jb.lee.v@gmail.com>
2019-04-09 15:59:37 +0900

    first exercise, output "Hello +name + nice to meet you

---
+++ ex1.py
@@ -0,0 +1,18 @@
+# Excercise for Programing 
+# Ex 1 
+
+# OUTPUT example 
+# What is your name? Brain 
+# Hello, Brain, nice to meet you
+
+# Limitation Condition 
+# seperate input part, string concatenation part, output part
+
+#input 
+name = input("What is your name? ")
+
+#string concatenation 
+answer = "Hello, " + name + ", nice to meet you."
+
+#output 
+print(answer)
